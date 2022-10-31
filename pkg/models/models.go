74ea838537d91959898bfbbbe078754630465549
KienPT <kientink26@gmail.com>
2022-10-31 23:39:09 +0700

    refactor

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -9,14 +9,6 @@ var ErrNoRecord = errors.New("models: no matching record found")
 var ErrDuplicateEmail = errors.New("models: duplicate email")
 var ErrInvalidCredentials = errors.New("models: invalid credentials")
 
-type Snippet struct {
-	ID      int
-	Title   string
-	Content string
-	Created time.Time
-	Expires time.Time
-}
-
 type User struct {
 	ID             int
 	Name           string
